d99e95596d5ea639cc067f1b2175fd16fc49cad4
Ciro Moura <ciro.dgm@gmail.com>
2020-07-28 12:32:18 -0300

    Alteração no slider

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,9 +6,23 @@ function CadastroCategoria() {
     return (
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
+
+            <form>
+
+                <label>
+                    Nome da Categoria:
+                    <input type="text" />
+                </label>
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
+
+
             <Link to="/">
-                Ir para Home
-            </Link>
+                Ir para home
+      </Link>
         </PageDefault>
     )
 }
