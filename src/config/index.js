f75c76f354c0fbb5effcd66b659a27f01e9e5954
Ciro Moura <ciro.dgm@gmail.com>
2020-08-01 00:47:09 -0300

    URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
     ? 'http://localhost:8080'
-    : 'https://guardavideos.herokuapp.com/categorias';
+    : 'https://guardavideos.herokuapp.com';
 
 export default {
     URL_BACKEND_TOP,
